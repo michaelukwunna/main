782f1732e442c79a57f35ac6fea1a51611066d7a
michaelukwunna <113518798+michaelukwunna@users.noreply.github.com>
2022-09-14 12:59:49 +0100

    Add files via upload

---
+++ home work.py
@@ -0,0 +1,11 @@
+nam1 = float(input("enter the first digit "))
+operrator1= input('enter opperator ')
+nam2 = float(input("enter the logic "))
+if operrator1 == '/':
+    print(nam1 / nam2)
+if operrator1 == '+':
+    print(nam1 + nam2)
+if operrator1 == '-':
+    print(nam1 - nam2)
+if operrator1 == '*':
+    print(nam1 * nam2)
